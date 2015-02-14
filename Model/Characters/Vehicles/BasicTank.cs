48c21fa823aa62802b56d0d7387f757062201b65
Georgi Petrov <georgipetrov02@gmail.com>
2015-02-14 11:17:24 +0200

    Collision improvement

--- Model/Characters/Vehicles/BasicTank.cs
+++ Model/Characters/Vehicles/BasicTank.cs
@@ -12,7 +12,6 @@
     {
 
         private float rotationAngle = 0f;
-        private Color color = Color.White;
         private SpriteBatch spriteBatch;
 
         public BasicTank(Texture2D objTexture, double positionX, double positionY, 
@@ -37,21 +36,18 @@
                 this.rect.Y += 5;
                 this.rotationAngle = (float)Math.PI;
                 CollisionDetector(Direction.Down);
-
             }
             else if (state.IsKeyDown(Keys.Left) && this.rect.X - this.objTexture.Width / 2 > 0)
             {
                 this.rect.X -= 5;
                 this.rotationAngle = 4.7123f;
                 CollisionDetector(Direction.Left);
-
             }
             else if (state.IsKeyDown(Keys.Right))
             {
                 this.rect.X += 5;
                 this.rotationAngle = 1.5707f;
                 CollisionDetector(Direction.Right);
-
             }
 
         }
@@ -60,7 +56,7 @@
         {
             spriteBatch.Draw(this.objTexture, null, this.rect, null,
                 new Vector2(this.objTexture.Width / 2, this.objTexture.Width / 2),
-                    rotationAngle, Vector2.Zero, color, SpriteEffects.None, 0f);
+                    rotationAngle, Vector2.Zero, Color.White, SpriteEffects.None, 0f);
             
         }
 
@@ -69,12 +65,11 @@
         {
             foreach (var gameObj in UltimateTankClash.Engine.GameEngine.GameObjects)
             {
-                if (this.rect.X < gameObj.rect.X + gameObj.rect.Width &&
-                   this.rect.X + this.rect.Width > gameObj.rect.X &&
-                   this.rect.Y < gameObj.rect.Y + gameObj.rect.Height &&
-                   this.rect.Height + this.rect.Y > gameObj.rect.Y)
+                if (this.rect.X - (this.objTexture.Width)/2 < gameObj.rect.X + gameObj.rect.Width &&
+                   this.rect.X + this.rect.Width - (this.objTexture.Width) / 2 > gameObj.rect.X &&
+                   this.rect.Y - (this.objTexture.Width) / 2 < gameObj.rect.Y + gameObj.rect.Height &&
+                   this.rect.Height + this.rect.Y - (this.objTexture.Width) / 2 > gameObj.rect.Y)
                 {
-
                     switch (direction)
                     {
                         case Direction.Up: this.rect.Y += 5; break;
@@ -82,12 +77,9 @@
                         case Direction.Left: this.rect.X += 5; break;
                         case Direction.Right: this.rect.X -= 5; break;
                     }
-                    this.color = Color.Red;
                 }
-                else this.color = Color.White;
             }
         }
-
 
     }
 }
