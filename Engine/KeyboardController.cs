6cee1aab37f28ee61b1ffdf42644f17ef1056b02
Domin1k <kristian.lubenov@gmail.com>
2015-02-25 13:56:15 +0200

    Pause implemented

---
+++ Engine/KeyboardController.cs
@@ -0,0 +1,24 @@
+﻿namespace UltimateTankClash.Engine
+{
+    using System;
+    using Interfaces;
+    using Microsoft.Xna.Framework.Input;
+
+    public class KeyboardController : IController
+    {
+        public event EventHandler Pause;
+        private KeyboardState keyboard;
+
+        public void ProcessUserInput()
+        {
+            this.keyboard = Keyboard.GetState();
+            if (this.keyboard.IsKeyDown(Keys.P))
+            {
+                if (this.Pause != null)
+                {
+                    this.Pause(this, new EventArgs());
+                }
+            }
+        }
+    }
+}
