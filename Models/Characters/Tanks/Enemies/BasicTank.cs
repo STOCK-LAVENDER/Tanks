86bcb6025c873d3ba10e63926658d76806f274ee
Filip Kolev <fil.kolev@gmail.com>
2015-02-25 12:30:27 +0200

    added bunkers

---
+++ Models/Characters/Tanks/Enemies/BasicTank.cs
@@ -0,0 +1,20 @@
+﻿namespace UltimateTankClash.Models.Characters.Tanks.Enemies
+{
+    using Microsoft.Xna.Framework;
+    using Microsoft.Xna.Framework.Graphics;
+
+    public class BasicTank : EnemyTank
+    {
+        private const int DefaultPhysicalAttack = 50;
+        private const int DefaultPhysicalDefense = 10;
+        private const int DefaultHealthPoints = 750;
+        private const int DefaultSpeed = 2;
+        private const int TimeBetweenDirectionSwitches = 100;
+        private const int TimeBetweenShots = 50;
+
+        public BasicTank(Texture2D objTexture, Rectangle rectangle)
+            : base(objTexture, rectangle, DefaultPhysicalAttack, DefaultPhysicalDefense, DefaultHealthPoints, DefaultSpeed, TimeBetweenDirectionSwitches, TimeBetweenShots)
+        {
+        }
+    }
+}
---
+++ Models/Characters/Tanks/Enemies/FastTank.cs
@@ -0,0 +1,20 @@
+﻿namespace UltimateTankClash.Models.Characters.Tanks.Enemies
+{
+    using Microsoft.Xna.Framework;
+    using Microsoft.Xna.Framework.Graphics;
+
+    class FastTank : EnemyTank
+    {
+        private const int DefaultPhysicalAttack = 25;
+        private const int DefaultPhysicalDefense = 10;
+        private const int DefaultHealthPoints = 250;
+        private const int DefaultSpeed = 4;
+        private const int TimeBetweenDirectionSwitches = 50;
+        private const int TimeBetweenShots = 25;
+
+        public FastTank(Texture2D objTexture, Rectangle rectangle)
+            : base(objTexture, rectangle, DefaultPhysicalAttack, DefaultPhysicalDefense, DefaultHealthPoints, DefaultSpeed, TimeBetweenDirectionSwitches, TimeBetweenShots)
+        {
+        }
+    }
+}
