2bd81e8901c82a13265c64dc49b01a09bf0b5693
Georgi Petrov <georgipetrov02@gmail.com>
2015-02-26 21:33:10 +0200

    Enemies added to instructions form

--- MenuItems/InstructionsForm.Designer.cs
+++ MenuItems/InstructionsForm.Designer.cs
@@ -8,7 +8,6 @@
         private Button backButton;
         private PictureBox controlsImage;
         private Label controlsLabel;
-        private PictureBox pictureBox1;
         private Label extraPacksLbl;
         private PictureBox pauseBtn;
         private PictureBox muteBtn;
@@ -44,14 +43,17 @@
             this.backButton = new System.Windows.Forms.Button();
             this.controlsImage = new System.Windows.Forms.PictureBox();
             this.controlsLabel = new System.Windows.Forms.Label();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.extraPacksLbl = new System.Windows.Forms.Label();
             this.pauseBtn = new System.Windows.Forms.PictureBox();
             this.muteBtn = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.EnemiesLbl = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.controlsImage)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pauseBtn)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.muteBtn)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // backButton
@@ -74,7 +76,7 @@
             this.controlsImage.ErrorImage = null;
             this.controlsImage.Image = ((System.Drawing.Image)(resources.GetObject("controlsImage.Image")));
             this.controlsImage.InitialImage = ((System.Drawing.Image)(resources.GetObject("controlsImage.InitialImage")));
-            this.controlsImage.Location = new System.Drawing.Point(36, 122);
+            this.controlsImage.Location = new System.Drawing.Point(12, 122);
             this.controlsImage.Name = "controlsImage";
             this.controlsImage.Size = new System.Drawing.Size(164, 163);
             this.controlsImage.TabIndex = 7;
@@ -86,22 +88,11 @@
             this.controlsLabel.BackColor = System.Drawing.Color.Transparent;
             this.controlsLabel.Font = new System.Drawing.Font("Impact", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.controlsLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(28)))), ((int)(((byte)(11)))));
-            this.controlsLabel.Location = new System.Drawing.Point(38, 65);
+            this.controlsLabel.Location = new System.Drawing.Point(5, 65);
             this.controlsLabel.Name = "controlsLabel";
             this.controlsLabel.Size = new System.Drawing.Size(162, 29);
             this.controlsLabel.TabIndex = 8;
             this.controlsLabel.Text = "Player Controls";
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
-            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(300, 105);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(275, 285);
-            this.pictureBox1.TabIndex = 9;
-            this.pictureBox1.TabStop = false;
             // 
             // extraPacksLbl
             // 
@@ -109,7 +100,7 @@
             this.extraPacksLbl.BackColor = System.Drawing.Color.Transparent;
             this.extraPacksLbl.Font = new System.Drawing.Font("Impact", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.extraPacksLbl.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(28)))), ((int)(((byte)(11)))));
-            this.extraPacksLbl.Location = new System.Drawing.Point(295, 65);
+            this.extraPacksLbl.Location = new System.Drawing.Point(177, 65);
             this.extraPacksLbl.Name = "extraPacksLbl";
             this.extraPacksLbl.Size = new System.Drawing.Size(127, 29);
             this.extraPacksLbl.TabIndex = 10;
@@ -119,7 +110,7 @@
             // 
             this.pauseBtn.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.pauseBtn.Image = global::UltimateTankClash.Properties.Resources.pause;
-            this.pauseBtn.Location = new System.Drawing.Point(36, 300);
+            this.pauseBtn.Location = new System.Drawing.Point(10, 300);
             this.pauseBtn.Name = "pauseBtn";
             this.pauseBtn.Size = new System.Drawing.Size(60, 60);
             this.pauseBtn.TabIndex = 11;
@@ -129,35 +120,75 @@
             // 
             this.muteBtn.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.muteBtn.Image = global::UltimateTankClash.Properties.Resources.mute;
-            this.muteBtn.Location = new System.Drawing.Point(140, 300);
+            this.muteBtn.Location = new System.Drawing.Point(114, 300);
             this.muteBtn.Name = "muteBtn";
             this.muteBtn.Size = new System.Drawing.Size(60, 60);
             this.muteBtn.TabIndex = 12;
             this.muteBtn.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Image = global::UltimateTankClash.Properties.Resources.consumables;
+            this.pictureBox1.Location = new System.Drawing.Point(182, 122);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(206, 272);
+            this.pictureBox1.TabIndex = 13;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.pictureBox2.Image = global::UltimateTankClash.Properties.Resources.tanks;
+            this.pictureBox2.Location = new System.Drawing.Point(401, 122);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(211, 272);
+            this.pictureBox2.TabIndex = 14;
+            this.pictureBox2.TabStop = false;
+            // 
+            // EnemiesLbl
+            // 
+            this.EnemiesLbl.AutoSize = true;
+            this.EnemiesLbl.BackColor = System.Drawing.Color.Transparent;
+            this.EnemiesLbl.Font = new System.Drawing.Font("Impact", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EnemiesLbl.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(28)))), ((int)(((byte)(11)))));
+            this.EnemiesLbl.Location = new System.Drawing.Point(396, 65);
+            this.EnemiesLbl.Name = "EnemiesLbl";
+            this.EnemiesLbl.Size = new System.Drawing.Size(101, 29);
+            this.EnemiesLbl.TabIndex = 15;
+            this.EnemiesLbl.Text = "Enemies:";
             // 
             // InstructionsForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(624, 442);
+            this.Controls.Add(this.EnemiesLbl);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.muteBtn);
             this.Controls.Add(this.pauseBtn);
             this.Controls.Add(this.extraPacksLbl);
-            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.controlsLabel);
             this.Controls.Add(this.controlsImage);
             this.Controls.Add(this.backButton);
             this.Name = "InstructionsForm";
             this.Text = "InstructionsForm";
             ((System.ComponentModel.ISupportInitialize)(this.controlsImage)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pauseBtn)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.muteBtn)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
+
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private Label EnemiesLbl;
     }
 }
